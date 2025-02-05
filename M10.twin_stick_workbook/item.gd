3e228ba6d36e44d4fc8bbdaae00aaf8809d20cd4
joszeto1 <joszeto1@s.sfusd.edu>
2025-02-05 13:03:20 -0800

    switch yuyu

---
+++ M10.twin_stick_workbook/item.gd
@@ -0,0 +1,7 @@
+class_name Item extends Resource
+
+@export var texture: Texture2D = null
+@export var sfx: AudioStream = null
+
+func use(player: Player) -> void:
+	pass
---
+++ M10.twin_stick_workbook/pickup.gd
@@ -0,0 +1,3 @@
+class_name Pickup extends Area2D
+
+#@export var item: Item = null: set = set_item
