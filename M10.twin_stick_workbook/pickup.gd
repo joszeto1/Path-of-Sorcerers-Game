6ce26d0401f3e41fb319859d7d2f08f2a4eff26e
Yujing14 <yuyu1@s.sfusd.edu>
2025-02-07 14:44:18 -0800

    Share

--- M10.twin_stick_workbook/pickup.gd
+++ M10.twin_stick_workbook/pickup.gd
@@ -1,3 +1,26 @@
+@tool
 class_name Pickup extends Area2D
 
-#@export var item: Item = null: set = set_item
+@onready var _sprite_2d: Sprite2D = %Sprite2D
+@onready var _collision_shape_2d: CollisionShape2D = %CollisionShape2D
+@onready var _audio_stream_player_2d: AudioStreamPlayer2D = %AudioStreamPlayer2D
+
+@export var item: Item = null
+
+func _ready() -> void:
+	#_sprite_2d.texture = item.texture
+	body_entered.connect(func (body: Node2D) -> void:
+		if body is Player:
+			item.use(body)
+			
+			queue_free()
+		_audio_stream_player_2d.play()
+		
+	)
+
+func set_item(value: Item) -> void:
+	item = value
+	if _sprite_2d != null:
+		_sprite_2d.texture = item.texture
+	if _audio_stream_player_2d != null:
+		_audio_stream_player_2d.stream = item.sfx
